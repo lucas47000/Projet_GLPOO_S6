969b0365b42d356f87d1216a20ab0b3a30d1ebfd
Lucas <lucasgarcia@hotmail.fr>
2021-06-26 22:29:46 +0200

    ajout retour menu playlists & livre

--- jMusicHub/src/interfaces/Console/ClearConsole.java
+++ jMusicHub/src/interfaces/Console/ClearConsole.java
@@ -4,33 +4,9 @@ import java.io.IOException;
 
 public class ClearConsole {
 	
-	public static void main(String[] Args){
-		ClearConsole.clear();
-	}
-	
-	static String penguinClearConsole = "clear"; 
-	static String windowsClearConsole = "cls"; 
-	 
-	static String os = System.getProperty("os.name");
-	
-	public static void clear(){
+public static void clear(){
 		
-		if(os.startsWith("Linux")){ 
-			try {
-				Runtime.getRuntime().exec(penguinClearConsole);
-			} catch (IOException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			} 
-		} 
-		else if(os.startsWith("Windows")){ 
-			try {
-				Runtime.getRuntime().exec(windowsClearConsole);
-			} catch (IOException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			} 
-		} 
+		System.out.print("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n");
 	}
 	
 }
